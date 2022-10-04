5058dc893f0cec8630f5f47528ead36d7b095ace
boyfrank5 <boyclaesen@gmail.com>
2022-10-04 15:03:23 +0200

    udpate

--- simpylc/start_client.sh
+++ simpylc/start_client.sh
@@ -2,7 +2,7 @@
 
 export LIBGL_ALLOW_SOFTWARE=1
 
-(cd simulations/car/control_client; python ./drivingAgent.py) | - less
+(cd simulations/car/control_client; python ./drivingAgent.py) 
 
 # (cd simulations/car/control_client; python ./hardcoded_client.py) > log.txt
 
