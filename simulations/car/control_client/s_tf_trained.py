6ac0f60e0ecdec2b7ef2ffade4eb1a6892865ac3
boyfrank5 <boyclaesen@gmail.com>
2022-10-04 21:00:38 +0200

    Update

--- simulations/car/control_client/s_tf_trained.py
+++ simulations/car/control_client/s_tf_trained.py
@@ -7,19 +7,19 @@ filename = '/Users/boyfrankclaesen/MakeAIWork/simulations/car/control_client/s_t
 sonar_data = np.loadtxt(filename, dtype=float, max_rows= 1000, usecols= (0,1,2,))
 sonar_steeringAngle = np.loadtxt(filename, dtype=float, max_rows= 1000, usecols= (3,))
 
-sonar_data_val = sonar_data[500:800]
-sonar_steeringAngle_val = sonar_steeringAngle[500:800]
+sonar_data_val = sonar_data[500:700]
+sonar_steeringAngle_val = sonar_steeringAngle[500:700]
 
 model = tf.keras.Sequential([
-  tf.keras.Input(shape=(3,)),
-  tf.keras.layers.Dense(16, activation='relu'),
-  tf.keras.layers.Dense(16, activation='relu'),
-  tf.keras.layers.Dense(1)
+  tf.keras.Input(shape=(3,)),   # input; sensoren 1 t/m 3.
+  tf.keras.layers.Dense(16, activation='relu'), # hidden layer nr1. Relu is het NN trainingsproces koppeld gewichten aan de output.
+  tf.keras.layers.Dense(16, activation='relu'), # hidden layer nr2.
+  tf.keras.layers.Dense(1)   # output; 1 x stuurhoek.
 ])
 
 #compiler2, configureren berekening
-model.compile(optimizer='adam',
-  loss=tf.keras.losses.MeanSquaredError(),
+model.compile(optimizer='adam',   # Adam is een vervanger van gradient decent met als voordeel deze minder 'snel' blijft hangen tussen bepaalde waarde. 
+  loss=tf.keras.losses.MeanSquaredError(),  # De loss functie verteld hoeveel de voorspelde output in het model verschilt met de 'echte' output. 
   metrics=['accuracy'])
 
 model.fit(sonar_data, sonar_steeringAngle, epochs=500)
